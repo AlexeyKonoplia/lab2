358101bbd926378b88e595ae029e5e3deb2edcdf
Алексей Конопля <112964950+AlexeyKonoplia@users.noreply.github.com>
2023-03-06 23:32:09 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -6,9 +6,10 @@ def div(a,b):
                 a[i+j] = abs(int(a[i+j]) - int(b[j]))
         i += 1
     return(a)
+
 def crc_32(line):
-    g = list('100000' + bin(int('04C11DB7', 16))[2:]) #Порождающий для CRC-32-IEEE 802.3
-    bin_line = list(''.join(bin(ord(x))[2:] for x in line)+'0'*(len(g)-1)) #Строка, записанная в двоичном виде
+    g = list('100000' + bin(int('04C11DB7', 16))[2:])
+    bin_line = list(''.join(bin(ord(x))[2:] for x in line)+'0'*(len(g)-1))
     string = div(bin_line, g)[-32:]
     arr = ''.join(list(map(str, string)))
     return hex(int(arr, 2))[2:]
